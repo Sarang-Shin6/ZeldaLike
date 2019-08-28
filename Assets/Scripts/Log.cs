1abdb454cc9232051656a175ca5d4ce284ca3d06
Sarang Shin <sarang.shin6@gmail.com>
2019-08-28 14:44:17 +1000

    first commit

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+
+    public Transform target, homePosition;
+    public float chaseRadius, attackRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius) // Inside Chase Radius but outside Attack Radius
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
