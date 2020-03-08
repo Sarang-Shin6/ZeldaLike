9beb5e36bf47a6e3a0c9e34aa907afd2bae073fb
Sarang <sarang.shin6@gmail.com>
2020-03-09 08:51:14 +1100

    Added Log goToSleep animation and transitions

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -33,11 +33,51 @@ public class Log : Enemy
                 && currentState != EnemyState.stagger)
             {
                 var tempVector3 = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-                myRigidBody.MovePosition(tempVector3);
-                ChangeState(EnemyState.walk);
-            }
 
+                ChangeAnim(tempVector3 - transform.position);
+                myRigidBody.MovePosition(tempVector3);
+
+                ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
+            }
         }
+        else if(Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("wakeUp", false);
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    private void ChangeAnim(Vector2 direction)
+    {
+        if(Math.Abs(direction.x) > Math.Abs(direction.y))
+        {
+            if(Math.Abs(direction.x) > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else
+        {
+            if (Math.Abs(direction.y) > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else
+            {
+                SetAnimFloat(Vector2.down);
+            }
+        }
+        Debug.Log(direction);
     }
 
     private void ChangeState(EnemyState newState)
